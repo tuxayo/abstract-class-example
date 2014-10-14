0e0a190e5cfc4749a6511d57f04c3d6bf161e637
tuxayo <victor@tuxayo.net>
2014-10-14 08:17:34 +0200

    Add Sum test for multuple sum

--- test/SumTest.java
+++ test/SumTest.java
@@ -12,4 +12,14 @@ public class SumTest {
 		assertEquals("(x+y)", formula.asString());
 		assertTrue(formula.asValue() == 6.5);
 	}
+
+	@Test
+	public void testCumulativeSum() {
+		Variable x = new Variable("x", 2);
+		Variable y = new Variable("y", 4);
+		Variable z = new Variable("z", 10);
+		Formula formula = new Sum(x, y, z);
+		assertEquals("(x+y+z)", formula.asString());
+		assertTrue(formula.asValue() == 16);
+	}
 }
